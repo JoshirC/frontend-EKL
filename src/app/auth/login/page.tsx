87e957bcc486cfab7afe2a0d6972a27c8c6ad67e
JoshirC <joshir.contreras@gmail.com>
2025-06-26 14:12:34 -0400

    fix: diseño de login v2

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import Image from "next/image";
 import { useMutation } from "@apollo/client";
 import { useRouter } from "next/navigation";
 import { LoginUserInput, LoginResponse } from "@/types/graphql";
@@ -89,137 +88,123 @@ export default function Login() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-orange-400 via-orange-300 to-yellow-200 py-4 px-6 sm:p-8">
-      <div className="bg-white shadow-md rounded-lg py-6 sm:py-8 px-4 sm:px-6 w-full max-w-6xl">
-        {showAlert && (
-          <div>
-            <Alert
-              type={alertType}
-              message={alertMessage}
-              onClose={() => setShowAlert(false)}
-              modal={true}
-            />
-          </div>
-        )}
-        <div className="flex flex-col lg:grid lg:grid-cols-3 gap-6 sm:gap-8">
-          {/* Columna Imagen */}
-          <div className="col-span-1 lg:col-span-2 flex items-center justify-center order-first lg:order-last">
-            <div className="rounded-lg overflow-hidden h-full  w-full">
-              <Image
-                src="/login.png"
-                alt="Imagen de inicio de sesión"
-                width={800}
-                height={600}
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-orange-400 via-orange-300 to-yellow-200 py-4 px-12">
+      <div className="bg-white shadow-md rounded-lg py-6 sm:py-18 px-4 sm:px-6 sm:w-1/4">
+        {/* Columna Formulario */}
+        <div className="col-span-1 flex flex-col justify-center mx-4 sm:mx-8">
+          <h1 className="text-2xl sm:text-3xl text-center font-bold text-gray-700">
+            ¡Bienvenido!
+          </h1>
+          <p className="text-sm text-center text-gray-600 mt-2">
+            Ingrese sus credenciales para el acceso al sistema.
+          </p>
+          {showAlert && (
+            <div className="mt-4">
+              <Alert
+                type={alertType}
+                message={alertMessage}
+                onClose={() => setShowAlert(false)}
+                modal={true}
               />
             </div>
-          </div>
-
-          {/* Columna Formulario */}
-          <div className="col-span-1 flex flex-col justify-center mx-4 sm:mx-8">
-            <h1 className="text-2xl sm:text-3xl text-center font-bold text-gray-700">
-              ¡Bienvenido!
-            </h1>
-            <p className="text-sm text-center text-gray-600 mt-2">
-              Ingrese sus credenciales.
-            </p>
-            <form onSubmit={handleSubmit}>
-              {/* RUT */}
-              <label className="block text-base sm:text-lg font-bold mb-2 mt-4">
-                Rut
-              </label>
-              <div className="flex items-center border border-gray-300 rounded">
-                <input
-                  type="text"
-                  placeholder="Ingrese su número de Rut"
-                  className="w-full px-3 py-2 text-sm sm:text-base focus:outline-none"
-                  value={rut}
-                  onChange={(e) => setRut(e.target.value.toLocaleUpperCase())}
-                  disabled={loading}
-                  required
-                />
-              </div>
-
-              {/* Contraseña */}
-              <label className="block text-base sm:text-lg font-bold mb-2 mt-4">
-                Contraseña
-              </label>
-              <div className="flex items-center border border-gray-300 rounded relative">
-                <input
-                  type={mostrarContrasena ? "text" : "password"}
-                  placeholder="Ingrese su contraseña"
-                  className="w-full px-3 py-2 text-sm sm:text-base focus:outline-none"
-                  value={contrasena}
-                  onChange={(e) => setContrasena(e.target.value)}
-                  disabled={loading}
-                  required
-                />
-                <button
-                  type="button"
-                  className="absolute right-3 text-sm text-gray-300 hover:text-orange-500"
-                  onClick={() => setMostrarContrasena(!mostrarContrasena)}
-                >
-                  {mostrarContrasena ? (
-                    <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="inline w-5 h-5 mr-1"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M13.875 18.825A10.05 10.05 0 0112 19c-5 0-9-4.03-9-9s4-9 9-9c1.02 0 2.01.17 2.94.48M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                        />
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M19.07 4.93l-14.14 14.14"
-                        />
-                      </svg>
-                    </>
-                  ) : (
-                    <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="inline w-5 h-5 mr-1"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M1.458 12C2.732 7.943 6.523 5 12 5c5.477 0 9.268 2.943 10.542 7-1.274 4.057-5.065 7-10.542 7-5.477 0-9.268-2.943-10.542-7z"
-                        />
-                        <circle
-                          cx="12"
-                          cy="12"
-                          r="3"
-                          stroke="currentColor"
-                          strokeWidth={2}
-                          fill="none"
-                        />
-                      </svg>
-                    </>
-                  )}
-                </button>
-              </div>
-
-              {/* Botón Ingresar */}
-              <button
-                type="submit"
-                className="mt-6 bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded w-full transition duration-300 text-sm sm:text-base disabled:opacity-50"
+          )}
+          <form onSubmit={handleSubmit}>
+            {/* RUT */}
+            <label className="block text-base sm:text-lg font-bold mb-2 mt-4">
+              Rut
+            </label>
+            <div className="flex items-center border border-gray-300 rounded">
+              <input
+                type="text"
+                placeholder="Ingrese su número de Rut"
+                className="w-full px-3 py-2 text-sm sm:text-base focus:outline-none"
+                value={rut}
+                onChange={(e) => setRut(e.target.value.toLocaleUpperCase())}
                 disabled={loading}
+                required
+              />
+            </div>
+
+            {/* Contraseña */}
+            <label className="block text-base sm:text-lg font-bold mb-2 mt-4">
+              Contraseña
+            </label>
+            <div className="flex items-center border border-gray-300 rounded relative">
+              <input
+                type={mostrarContrasena ? "text" : "password"}
+                placeholder="Ingrese su contraseña"
+                className="w-full px-3 py-2 text-sm sm:text-base focus:outline-none"
+                value={contrasena}
+                onChange={(e) => setContrasena(e.target.value)}
+                disabled={loading}
+                required
+              />
+              <button
+                type="button"
+                className="absolute right-3 text-sm text-gray-300 hover:text-orange-500"
+                onClick={() => setMostrarContrasena(!mostrarContrasena)}
               >
-                {loading ? "Cargando..." : "Ingresar"}
+                {mostrarContrasena ? (
+                  <>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="inline w-5 h-5 mr-1"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M13.875 18.825A10.05 10.05 0 0112 19c-5 0-9-4.03-9-9s4-9 9-9c1.02 0 2.01.17 2.94.48M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M19.07 4.93l-14.14 14.14"
+                      />
+                    </svg>
+                  </>
+                ) : (
+                  <>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="inline w-5 h-5 mr-1"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M1.458 12C2.732 7.943 6.523 5 12 5c5.477 0 9.268 2.943 10.542 7-1.274 4.057-5.065 7-10.542 7-5.477 0-9.268-2.943-10.542-7z"
+                      />
+                      <circle
+                        cx="12"
+                        cy="12"
+                        r="3"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        fill="none"
+                      />
+                    </svg>
+                  </>
+                )}
               </button>
-            </form>
-          </div>
+            </div>
+
+            {/* Botón Ingresar */}
+            <button
+              type="submit"
+              className="mt-6 bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4 rounded w-full transition duration-300 text-sm sm:text-base disabled:opacity-50"
+              disabled={loading}
+            >
+              {loading ? "Cargando..." : "Ingresar"}
+            </button>
+          </form>
         </div>
       </div>
     </div>
